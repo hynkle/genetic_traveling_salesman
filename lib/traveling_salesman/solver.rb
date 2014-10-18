cfdffbf1fbf5f98da190f0a13936a993e862bd39
Jonathan Hinkle <hynkle@gmail.com>
2014-10-18 23:40:36 +0000

    add callbacks to solution process
    enables you to (for example) print out a summary at each generation

--- lib/traveling_salesman/solver.rb
+++ lib/traveling_salesman/solver.rb
@@ -13,13 +13,35 @@ module TravelingSalesman
 
     def solve!
       generations << Population.initial(@cities)
+      on_generation_callback.call(last_generation, generations.size)
       2.upto(@number_of_generations) do |generation_number|
-        generations << last_generation.successor
+        successor = last_generation.successor
+        generations << successor
+        on_generation_callback.call(successor, generations.size)
       end
+      after_completion_callback.call(last_generation)
     end
 
     def last_generation
       generations.last
+    end
+
+    def on_each_generation(&block)
+      @on_generation_callback = block
+    end
+
+    def after_completion(&block)
+      @after_completion_callback = block
+    end
+
+    private
+    
+    def on_generation_callback
+      @on_generation_callback ||= Proc.new{}
+    end
+
+    def after_completion_callback
+      @after_completion_callback ||= Proc.new{}
     end
 
   end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -30,4 +30,49 @@ describe Solver do
     end
   end
 
+  describe "#on_each_generation" do
+
+    let(:counter) { double }
+
+    it "it sets up a callback that's executed once per generation" do
+      expect(counter).to receive(:poke).exactly(number_of_generations).times
+      subject.on_each_generation do
+        counter.poke
+      end
+
+      subject.solve!
+    end
+
+    it "yields the population and the generation number" do
+      generation_counts = []
+      subject.on_each_generation do |population, generation_count|
+        expect(population).to be_a Population
+        generation_counts << generation_count
+      end
+
+      subject.solve!
+
+      expect(generation_counts).to eq (1..number_of_generations).to_a
+    end
+  end
+
+  describe "#after_completion" do
+
+    let(:counter) { double }
+
+    specify do
+      last_generation = nil
+      subject.after_completion do |final|
+        counter.poke
+        last_generation = final
+      end
+      expect(counter).to receive(:poke).exactly(1).times
+
+      subject.solve!
+
+      expect(last_generation).to eq(subject.last_generation)
+    end
+
+  end
+
 end
